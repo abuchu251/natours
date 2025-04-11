ac485fd6f92f4449e32e3f3fad659282cc585dbc
abuchu251 <samuelkendie@gmail.com>
2025-04-12 00:43:09 +0900

    Dealing with mongoose

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const AppError = require('./utils/AppError');
+const errorController = require('./controllers/errorController');
 
 const app = express();
 if (process.env.NODE_ENV === 'development') {
@@ -11,5 +13,15 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
+
+app.use('*', (req, res, next) => {
+  next(
+    new AppError(
+      `can't find the route ${req.originalUrl} from the server`,
+      404,
+    ),
+  );
+});
+app.use(errorController);
 
 module.exports = app;
